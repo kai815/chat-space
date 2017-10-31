3b18a69801db19617a5edffff60150c63598b890
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-10-31 19:28:36 +0900

    Make users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params) if current_user.id = user.id
+
+    redirect_to  "/"
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :email)
+  end
+
+end
