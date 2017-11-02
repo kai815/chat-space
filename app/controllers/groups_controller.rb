e24f8bd10fe9e38e431d9479adeceed3c99ab27c
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-02 19:32:40 +0900

    Set groups_controller_function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,18 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
   def create
+    Group.create(create_group)
   end
   def edit
   end
   def update
+  end
+
+  private
+  def create_group
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
