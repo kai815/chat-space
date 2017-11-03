6f291c53d4a8425059d94b210fddff4335895499
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-03 18:26:51 +0900

    Set messages_controller_details

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,21 @@
 class MessagesController < ApplicationController
-  def new
+
+  def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.save
+    redirecto_to group_messages_path
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
 end
