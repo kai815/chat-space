5a550100deb120bc0818a3f39d2038431794351f
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-04 15:59:29 +0900

    Set variation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,11 @@ class Message < ApplicationRecord
   belongs_to :group
 
   mount_uploader :image, ImageUploader
+
+  validates :body_or_image, presence: true
+
+  def body_or_image
+    body.presence || image.presence
+  end
+
 end
