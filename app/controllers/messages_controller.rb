9c7e9412d713dfdbd7ba0e74ddbc712bc4286a93
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-14 16:57:24 +0900

    Set son

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-     redirect_to group_messages_path, notice:"メッセージを送信しました"
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(params[:group_id]) }
+        format.json
+      end
     else
      flash.now[:alert] = "メッセージを入力してください"
      render :index
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.body @message.body
+json.user_name @message.user.name
+json.created_at @message.created_at
+json.image @message.image
