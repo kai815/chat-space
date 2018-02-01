ccaee15c75ac84f399ed9193b18e741cdab3dbca
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2018-02-01 20:11:33 +0900

    Modify time-showing

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,4 +10,7 @@ class Message < ApplicationRecord
     body.presence || image.presence
   end
 
+  def time
+    created_at.strftime("%Y年%m月%d日 %H時%M分")
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,13 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+
     config.generators do |g|
       g.javascripts false
       g.helper false
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
   end
 end
